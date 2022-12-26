59854c046ee7ea9d50830b860a9c1020ffa832c9
macnev2013 <macnev2013@gmail.com>
2022-12-26 11:33:18 +0530

    updated ci

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ def add_nums(a, b):
 def sub_nums(a, b):
     return a - b
 
-def test_add_nums():
-    time.sleep(3)
-    assert add_nums(1, 2) == 3
-    assert add_nums(1, 3) == 4
-    assert add_nums(1, 4) != 6
+class TestNumOperations:
+    def test_add_nums(self):
+        time.sleep(3)
+        assert add_nums(1, 2) == 3
+        assert add_nums(1, 3) == 4
+        assert add_nums(1, 4) != 6
 
-def test_sub_nums():
-    time.sleep(2)
-    assert sub_nums(1, 2) == -1
-    assert sub_nums(1, 3) == -2
-    assert sub_nums(1, 4) != 6
+    def test_sub_nums(self):
+        time.sleep(2)
+        assert sub_nums(1, 2) == -1
+        assert sub_nums(1, 3) == -2
+        assert sub_nums(1, 4) != 6
