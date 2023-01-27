53683f3555e4e8e81df9e832b442fced55590e16
Macwan Nevil <macnev2013@gmail.com>
2023-01-27 21:33:44 +0530

    pipeline for circle ci test insights  (#1)

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+import time
+
 def add_nums(a, b):
     return a + b
+
 
 def sub_nums(a, b):
     return a - b
 
-def test_add_nums():
-    assert add_nums(1, 2) == 3
-    assert add_nums(1, 3) == 4
-    assert add_nums(1, 4) != 6
+class TestNumOperations:
+    def test_add_nums(self):
+        time.sleep(3)
+        assert add_nums(1, 2) == 3
+        assert add_nums(1, 3) == 4
+        assert add_nums(1, 4) != 6
 
-def test_sub_nums():
-    assert sub_nums(1, 2) == -1
-    assert sub_nums(1, 3) == -2
-    assert sub_nums(1, 4) != 6
+    def test_sub_nums(self):
+        time.sleep(2)
+        assert sub_nums(1, 2) == -1
+        assert sub_nums(1, 3) == -2
+        assert sub_nums(1, 4) != 6
